38a2fdb8084bd7340c2bcb3922242ab8da9548a4
Edgar Sanchez <edsa292@vidalibarraquer.net>
2023-05-12 10:23:41 +0200

    Push Mètode Sumar

--- src/Suma.java
+++ src/Suma.java
@@ -1,3 +1,10 @@
 public class Suma {
+    public int num1;
+    public int num2;
+
+    // Mètode per Suma dos números i retornar el resultat
+    public int sumar() {
+        return num1 + num2;
+    }
 
 }
